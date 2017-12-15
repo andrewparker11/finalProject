5b0fc1b51e7b797ec9cc46d2f93b13179aae74d4
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-15 14:49:53 -0500

    Updated from Brython Server: 12/15/2017 2:49:52 PM

--- gameOfLife.py
+++ gameOfLife.py
@@ -1,1 +1,15 @@
+#Andrew Parker
+#Dec.15,2017
+#Game of Life by John Conway
 
+from ggame import *
+
+#buildBoard
+def buildBoard():
+    board = [['a','b','c'],['d','e','f'],['g','h','i']]
+     
+def reDrawAll():
+    for row in range(0,3):
+        for col in range(0,3):
+            sprite(board[row][col],' ',end = '') #end is keyword
+        sprite()
