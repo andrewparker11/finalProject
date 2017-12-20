e0a2b5a7ab83ce16a6568257319211d651f3a9b5
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-20 13:33:45 -0500

    Updated from Brython Server: 12/20/2017 1:33:44 PM

--- gameOfLife.py
+++ gameOfLife.py
@@ -19,9 +19,9 @@ def reDrawAll():
     for row in range(0,10):
         for col in range(0,10):
             if board[row][col] == 0:
-                sprite(whiteSquare((300,100))
+                sprite(whiteSquare,(300,100))
             if board[row][col] == 1:
-                sprite(blackSquare(300,200))
+                sprite(blackSquare,(300,200))
                 
 #sprite(board[row][col],' ',end = '') #end is keyword
 
