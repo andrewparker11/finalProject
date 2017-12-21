fc1fce8c2563869e0bfead80e7566cb8a81c913a
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-21 16:58:10 -0500

    Updated from Brython Server: 12/21/2017 4:58:10 PM

--- gameOfLife.py
+++ gameOfLife.py
@@ -23,11 +23,26 @@ def reDrawAll():
             if data['Board'][row][col] == 1:
                 Sprite(blackSquare,(30*row+300,30*col+100))
     
-#def mouseclick():
     
+def mouseclick():
+    if event.x > 0 and event.x < 40 and event.y > 0 and event.y < 40:
+        Sprite(blackSquare,(0,0))
+    elif event.x > 0 and event.x < 40 and event.y > 40 and event.y < 80:
+        Sprite(blackSquare,(0,40))
+    elif event.x > 0 and event.x < 40 and event.y > 80 and event.y < 120:
+        Sprite(blackSquare,(0,80))
+    elif event.x > 0 and event.x < 40 and event.y > 120 and event.y < 160:
+        Sprite(blackSquare,(0,120))
+    elif event.x > 0 and event.x < 40 and event.y > 160 and event.y < 200:
+        Sprite(blackSquare,(0,160))
+    elif event.x > 0 and event.x < 40 and event.y > 200 and event.y < 240:
+        Sprite(blackSquare,(0,200))
+    elif event.x > 0 and event.x < 40 and event.y > 240 and event.y < 280:
+        Sprite(blackSquare,(0,240))
+    elif event.x > 0 and event.x < 40 and event.y > 280 and event.y < 320:
+        Sprite(blackSquare,(0,280))
 
 #sprite(board[row][col],' ',end = '') #end is keyword
-
 
 
 
