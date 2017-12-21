65d7e9bbe7ab53dd45e583e35e80757b10272009
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2017-12-21 16:40:43 -0500

    Updated from Brython Server: 12/21/2017 4:40:42 PM

--- gameOfLife.py
+++ gameOfLife.py
@@ -23,16 +23,15 @@ def reDrawAll():
             if data['Board'][row][col] == 1:
                 Sprite(blackSquare,(30*row+300,30*col+100))
     
-def mouseclick():
+#def mouseclick():
     
-
 
 #sprite(board[row][col],' ',end = '') #end is keyword
 
 
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     
     data = {}
     data['Board'] = buildBoard()
