5ac2be16fb3dd7304b5d41c93e4829f3687495a2
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2018-01-02 14:42:42 -0500

    Updated from Brython Server: 1/2/2018 2:42:42 PM

--- gameOfLife.py
+++ gameOfLife.py
@@ -20,14 +20,20 @@ def reDrawAll():
         for col in range(0,10):
             if data['Board'][row][col] == '0':
                 Sprite(whiteSquare,(50*row,50*col))
-            if data['Board'][row][col] == 1:
+            if data['Board'][row][col] == '1':
                 Sprite(blackSquare,(50*row,50*col))
     
     
 def mouseClick(event):
     rx = event.x//50 
     ry = event.y//50
-    data['Board'][rx][ry] = 1
+    data['Board'][rx][ry] = '1'
+
+def numNeighbors():
+    
+    
+def nextGeneration():
+
 
 
 #sprite(board[row][col],' ',end = '') #end is keyword
@@ -41,7 +47,7 @@ if __name__ == '__main__':
     data['frames'] = 0
     
     reDrawAll()
-
+    numNeighbors()
 
     App().run()
     App().listenMouseEvent('click',mouseClick)
