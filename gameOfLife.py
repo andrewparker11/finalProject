4e75ed9382702d6abe934d2365eeb43e0c63f3d4
andrewparker11 <31515304+andrewparker11@users.noreply.github.com>
2018-01-04 11:34:25 -0500

    Updated from Brython Server: 1/4/2018 11:34:24 AM

--- gameOfLife.py
+++ gameOfLife.py
@@ -37,15 +37,17 @@ def reDrawAll():
             Sprite(startText,(575,405))
     
 def mouseClick(event):
-    rx = event.x//50 
-    ry = event.y//50
-    data['Board'][rx][ry] = '1'
-    reDrawAll()
-    
     if event.x > 550 and event.x < 650:
         if event.y > 400 and event.y < 450:
             numNeigbors(row,col) 
             nextGeneration()
+    else:    
+        rx = event.x//50 
+        ry = event.y//50
+        data['Board'][rx][ry] = '1'
+        reDrawAll()
+    
+    
 
 
 def numNeighbors(row,col):
